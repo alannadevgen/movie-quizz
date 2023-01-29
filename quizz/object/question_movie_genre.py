37a3b7661e2f23d2b3d76d47f32aea8739d72277
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-01-29 18:27:01 +0100

    Question movie genre

---
+++ quizz/object/question_movie_genre.py
@@ -0,0 +1,35 @@
+from quizz.object.abstract_question import AbstractQuestion
+from quizz.object.question_movie import QuestionMovie
+
+
+class QuestionMovieYear(QuestionMovie, AbstractQuestion):
+    def __init__(
+            self,
+            movie_id,
+            title,
+            year,
+            genre,
+            duration,
+            avg_vote,
+            critics_vote,
+            public_vote,
+            total_votes
+        ) -> None:
+        super().__init__(
+            movie_id,
+            title,
+            year,
+            genre,
+            duration,
+            avg_vote,
+            critics_vote,
+            public_vote,
+            total_votes
+        )
+        self.question_type = "movie year"
+
+    def display_question(self):
+        return f"What is the main genre of the film {self.title}?"
+
+    def get_correct_answer(self):
+        return self.genre
