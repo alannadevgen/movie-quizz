0a13a8a36dcbc42354064b85a519bc9fa25b217d
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-13 20:59:55 +0100

    Minimal structure for question actor

--- quizz/object/question_actor.py
+++ quizz/object/question_actor.py
@@ -2,5 +2,11 @@ from quizz.object.abstract_question import AbstractQuestion
 
 
 class QuestionActor(AbstractQuestion):
-    def __init__(self) -> None:
+    def __init__(self, actor_id) -> None:
         super().__init__()
+    
+    def display_question(self):
+        return super().display_question()
+
+    def get_correct_answer(self):
+        return super().get_correct_answer()
