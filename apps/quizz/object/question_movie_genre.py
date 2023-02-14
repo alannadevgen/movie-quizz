a4aec48049d2181586f89aad983bc8e2ce217829
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-14 12:27:09 +0100

    Question movie genre

--- apps/quizz/object/question_movie_genre.py
+++ apps/quizz/object/question_movie_genre.py
@@ -1,31 +1,16 @@
-from object.abstract_question import AbstractQuestion
-from object.question_movie import QuestionMovie
+from apps.quizz.object.abstract_question import AbstractQuestion
+from apps.quizz.object.question_movie import QuestionMovie
+from apps.quizz import dao
 
 
 class QuestionMovieGenre(QuestionMovie, AbstractQuestion):
-    def __init__(
-            self,
-            movie_id,
-            title,
-            year,
-            genre,
-            duration,
-            avg_vote,
-            critics_vote,
-            public_vote,
-            total_votes
-        ) -> None:
-        super().__init__(
-            movie_id,
-            title,
-            year,
-            genre,
-            duration,
-            avg_vote,
-            critics_vote,
-            public_vote,
-            total_votes
-        )
+    def __init__(self) -> None:
+        random_id = dao.get_random_id("movies", "movie_id")
+        infos_movie = dao.get_info_movies_id(random_id)
+        self.title = infos_movie['title']
+        self.genre = infos_movie['genre']
+        self.correct_letter = None
+        self.incorrect_answers = []
         self.question_type = "movie genre"
 
     def display_question(self):
@@ -33,3 +18,9 @@ class QuestionMovieGenre(QuestionMovie, AbstractQuestion):
 
     def get_correct_answer(self):
         return self.genre
+    
+    def get_incorrect_answers(self):
+        '''
+        Generate random answers for the question
+        '''
+        return dao.random_bad_answers(name_col="genre", true_value=self.genre)
