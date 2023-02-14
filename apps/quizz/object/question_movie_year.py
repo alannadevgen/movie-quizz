64494b9239a876bad821a12f9611e592903d9002
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-14 14:08:22 +0100

    Question movie year

--- apps/quizz/object/question_movie_year.py
+++ apps/quizz/object/question_movie_year.py
@@ -1,38 +1,26 @@
 from object.abstract_question import AbstractQuestion
 from object.question_movie import QuestionMovie
+import dao
 
 
 class QuestionMovieYear(QuestionMovie, AbstractQuestion):
-    def __init__(
-            self,
-            movie_id,
-            title,
-            year,
-            genre,
-            duration,
-            avg_vote,
-            critics_vote,
-            public_vote,
-            total_votes
-        ) -> None:
-        super().__init__(
-            movie_id,
-            title,
-            year,
-            genre,
-            duration,
-            avg_vote,
-            critics_vote,
-            public_vote,
-            total_votes
-        )
+    def __init__(self) -> None:
+        random_id = dao.get_random_id("movies", "movie_id")
+        infos_movie = dao.get_info_movies_id(random_id)
+        self.title = infos_movie['title']
+        self.year = infos_movie['year']
+        self.correct_letter = None
+        self.incorrect_answers = []
         self.question_type = "movie year"
 
     def display_question(self):
-        return f"When was {self.title} released?"
+        return f"When was the film {self.title} released?"
 
     def get_correct_answer(self):
         return self.year
-
+    
     def get_incorrect_answers(self):
-        return super().get_incorrect_answers()
+        '''
+        Generate random answers for the question
+        '''
+        return dao.random_bad_answers_int(name_col="year", true_value=self.year)
