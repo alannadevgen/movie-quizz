711604ca62153520aa9c9c87399590d29796dabc
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-14 12:27:43 +0100

    Creating quizz

--- apps/quizz/main.py
+++ apps/quizz/main.py
@@ -1,14 +1,19 @@
-from object.question_actor import QuestionActor
+from object.question_movie_genre import QuestionMovieGenre
 
-nb_questions = input('How many questions do you want ?')
+nb_questions = input('How many questions do you want ? ')
 
-for _ in range(int(nb_questions)):
-    question_actor = QuestionActor()
-    res_letter = input(question_actor.display_question())
+nb_correct_answers = 0
+for num_question in range(int(nb_questions)):
+    print(f'\n----- Question {num_question+1} ------')
+    question = QuestionMovieGenre()
+    res_letter = input(question.display_full_question())
 
-    answer = question_actor.get_correct_answer()
-
-    if res_letter.lower() != answer.lower():
-        print(f'False :(\n The correct answer is {answer}')
+    correct_letter = question.get_correct_letter()
+    answer = question.get_correct_answer()
+    
+    if res_letter.lower() != correct_letter.lower():
+        print(f'False :(\nThe correct answer is {answer}\n')
     else:
-        print('Correct !')
+        nb_correct_answers += 1
+        print('Correct!\n')
+print(f'You got {nb_correct_answers} correct anwsers!\n')
