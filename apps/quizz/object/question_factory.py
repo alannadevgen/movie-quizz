1060e9930105867641a0746ec94b181b988ef157
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-14 18:18:32 +0100

    Updates types for question factory

--- apps/quizz/object/question_factory.py
+++ apps/quizz/object/question_factory.py
@@ -1,4 +1,6 @@
 from object.question_actor import QuestionActor
+from object.question_movie_country import QuestionMovieCountry
+from object.question_movie_director import QuestionMovieDirector
 from object.question_movie_genre import QuestionMovieGenre
 from object.question_movie_year import QuestionMovieYear
 import random
@@ -7,7 +9,7 @@ import random
 class QuestionFactory():
     @staticmethod
     def instanciate_question():
-        all_types = ["actor", "movie genre", "movie year"]
+        all_types = ["actor", "movie genre", "movie year", "movie country", "movie director"]
         question_type = random.choice(all_types)
 
         if question_type == "actor":
@@ -16,5 +18,8 @@ class QuestionFactory():
             question = QuestionMovieGenre()
         elif question_type == "movie year":
             question = QuestionMovieYear()
-        
+        elif question_type == "movie country":
+            question = QuestionMovieCountry()
+        elif question_type == "movie director":
+            question = QuestionMovieDirector()
         return question
